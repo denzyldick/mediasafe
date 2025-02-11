16f982ee23c9670e544e642118fd563b6b6c73c4
Denzyl Dick <2477646+denzyldick@users.noreply.github.com>
2025-02-11 18:29:57 +0100

    Added devtools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 fn main() {
     #[cfg(debug_assertions)]
-    //  let devtools = tauri_plugin_devtools::init(); // initialize the plugin as early as possible
-    let builder = tauri::Builder::default()
+    let devtools = tauri_plugin_devtools::init(); // initialize the plugin as early as possible
+    let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init());
-
-    //    builder = builder.plugin(devtools); // then register it with Tauri
+    builder = builder.plugin(devtools); // then register it with Tauri
 
     builder
         .run(tauri::generate_context!())
